f8f6eddfbf7da6d17db505e57499ae5e987f9062
unknown <ckelly28@myseneca.ca>
2019-12-18 13:54:55 -0500

    Some PsuedoCoding done

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,29 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bulls and Cows!"));
+    PrintLine(TEXT("Please guess the four letter isogram")); //TODO Fix magic number
+
+    //NEED Set Lives
+    //NEED Set Difficulty (WordLength)
+    HiddenWord = TEXT("skate"); //TODO Make this not hard coded
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    PrintLine(Input);
+    if(Input == HiddenWord){
+        PrintLine(TEXT("You got it! Congratulations!"));
+    }
+    else{
+        PrintLine(TEXT("That's incorrect, try again"));
+    }
+        //NEED Check isogram
+        //NEED Right amount of characters
+        //NEED Check if lives > 0
+        //NEED play again if lives > 0
+        //NEED remove life
 
 }
